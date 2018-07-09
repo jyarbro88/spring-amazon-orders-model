19587df54dd4240d594ad147280f74a0c6325d26
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-09 15:25:20 -0500

    added product bean

---
+++ src/main/java/com/spring/amazondatamodel/weblayer/beans/ProductBean.java
@@ -0,0 +1,52 @@
+package com.spring.amazondatamodel.weblayer.beans;
+
+import java.sql.Blob;
+
+public class ProductBean {
+
+    private Long id;
+    private String name;
+    private String description;
+    private Blob image;
+    private Double price;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Blob getImage() {
+        return image;
+    }
+
+    public void setImage(Blob image) {
+        this.image = image;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
