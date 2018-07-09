ffc259be7ac0205cd3bbf0ae7f083b18c7e37376
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-09 15:33:47 -0500

    added shipment bean

---
+++ src/main/java/com/spring/amazondatamodel/weblayer/beans/ShipmentBean.java
@@ -0,0 +1,52 @@
+package com.spring.amazondatamodel.weblayer.beans;
+
+import java.util.Date;
+
+public class ShipmentBean {
+
+    private Long id;
+    private AddressBean addressBean;
+    private OrderLineItemBean orderLineItemBean;
+    private Date shippedDate;
+    private Date deliveryDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public AddressBean getAddressBean() {
+        return addressBean;
+    }
+
+    public void setAddressBean(AddressBean addressBean) {
+        this.addressBean = addressBean;
+    }
+
+    public OrderLineItemBean getOrderLineItemBean() {
+        return orderLineItemBean;
+    }
+
+    public void setOrderLineItemBean(OrderLineItemBean orderLineItemBean) {
+        this.orderLineItemBean = orderLineItemBean;
+    }
+
+    public Date getShippedDate() {
+        return shippedDate;
+    }
+
+    public void setShippedDate(Date shippedDate) {
+        this.shippedDate = shippedDate;
+    }
+
+    public Date getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Date deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+}
