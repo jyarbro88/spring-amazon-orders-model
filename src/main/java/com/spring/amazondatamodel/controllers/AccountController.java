2cd9a19e10fcdde06e4979a1a32cdcb62c61f75c
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-10 11:16:39 -0500

    removed unused files

--- src/main/java/com/spring/amazondatamodel/controllers/AccountController.java
+++ src/main/java/com/spring/amazondatamodel/controllers/AccountController.java
@@ -1,4 +1,4 @@
-package com.spring.amazondatamodel.weblayer.controllers;
+package com.spring.amazondatamodel.controllers;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.amazondatamodel.datalayer.Account;
