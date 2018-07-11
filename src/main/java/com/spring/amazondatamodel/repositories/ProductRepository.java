046064b27ee08caca45703d2914f69f0409f290f
Joe Yarbrough <jyarbrough@solstice.com>
2018-07-10 23:23:57 -0500

    cleaning up repo

--- src/main/java/com/spring/amazondatamodel/repositories/ProductRepository.java
+++ src/main/java/com/spring/amazondatamodel/repositories/ProductRepository.java
@@ -2,8 +2,6 @@ package com.spring.amazondatamodel.repositories;
 
 import com.spring.amazondatamodel.datalayer.ProductDAO;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 
 import javax.transaction.Transactional;
 import java.util.Optional;
